50786340006de6fe248dbe74a68115783387fdc7
Nitesh Kumar <niteshk@geekyants.com>
2023-01-21 20:32:36 +0530

    feat: Added CompositeDisposable code to help dispose multiple DisposableObservers

--- app/src/main/java/com/example/rxjava/MainActivity.java
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.observers.DisposableObserver;
 import io.reactivex.rxjava3.schedulers.Schedulers;
@@ -27,7 +29,11 @@ public class MainActivity extends AppCompatActivity {
     // private Observer<String> myObserver;
 
     /// we change our Observer to a DisposableObserver type
-    private DisposableObserver<String> myObserver;
+    private DisposableObserver<String> myObserver, myObserver2;
+
+    /// a CompositeDisposable, can help us dispose a stream of Observers,
+    /// this way, we will not have to call dispose() methods on all Observers one by one
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     /// we create our disposable, this will help us to dispose all the subscriptions when this activity is disposed
     // private Disposable disposable;
@@ -102,6 +108,28 @@ public class MainActivity extends AppCompatActivity {
         /// we attach the observer to the observable
         /// this is how our observer will start getting data emitted from our observable
         myObservable.subscribe(myObserver);
+        /// whenever we attach an Observer, we add it to out compositeDisposable, to dispose it later
+        compositeDisposable.add(myObserver);
+
+        /// we create one more Observer
+        myObserver2 = new DisposableObserver<String>() {
+            @Override
+            public void onNext(@NonNull String s) {
+                Toast.makeText(MainActivity.this, "Observer 2 " + s, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+        myObservable.subscribe(myObserver2);
+        compositeDisposable.add(myObserver2);
 
         /// here we are not setting any schedulers(threads), so it will all happen in our main thread
     }
@@ -115,6 +143,8 @@ public class MainActivity extends AppCompatActivity {
         // disposable.dispose();
 
         /// now, we can directly call the dispose method of the `DisposableObserver`
-        myObserver.dispose();
+        // myObserver.dispose();
+
+        compositeDisposable.clear();
     }
 }
